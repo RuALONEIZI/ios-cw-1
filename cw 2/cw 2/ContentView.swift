ed6db74147cd8a2a1765597c524e74df141e7844
RuALONEIZI <100702655+RuALONEIZI@users.noreply.github.com>
2022-03-02 20:35:56 +0300

    حطيت اسمي

---
+++ cw 2/cw 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw 2
+//
+//  Created by Raghad Al-Oneizi on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello i am ruaa!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw 2/cw 2/cw_2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw_2App.swift
+//  cw 2
+//
+//  Created by Raghad Al-Oneizi on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
